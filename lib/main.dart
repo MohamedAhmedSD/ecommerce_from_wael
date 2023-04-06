2e2f1595ed9b69e61772b8b36461957e122bb6a5
MohamedAhmedSD <btoox8python@gmail.com>
2023-04-06 02:03:31 +0200

    just copy on boarding fiels and upgrade packages

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:ecommerce_wael/view/screen/onboarding.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+// [day 1]
+// upgrade packages => flutter pub upgrade --major-versions
+// its main folder are => core, data, controller, view
+
+// we can search about ui inside website our google images
+// then use screen shot to make our images
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const OnBoarding(),
+    );
+  }
+}
---
+++ lib/view/screen/onboarding.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/onboarding/custom_button.dart';
+import '../widgets/onboarding/custom_slider.dart';
+import '../widgets/onboarding/dot_controller.dart';
+
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(children: [
+            const Expanded(
+              flex: 3,
+              child: CustomSliderOnBoarding(),
+            ),
+            Expanded(
+                flex: 1,
+                child: Column(
+                  children: const [
+                    CustomDotControllerOnBoarding(),
+                    Spacer(flex: 2),
+                    CustomButtonOnBoarding()
+                  ],
+                ))
+          ]),
+        ));
+  }
+}
