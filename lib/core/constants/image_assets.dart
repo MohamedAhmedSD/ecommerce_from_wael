d7baa8e38ea9d19dafdf8ccf850de95bfc7a9eb0
MohamedAhmedSD <btoox8python@gmail.com>
2023-04-08 20:32:11 +0200

    loginpage ..

--- lib/core/constants/image_assets.dart
+++ lib/core/constants/image_assets.dart
@@ -11,6 +11,7 @@ class AppImageAsset {
   static const String onBoardingImageOne = "$rootImages/one.PNG";
   static const String onBoardingImageTwo = "$rootImages/two.PNG";
   static const String onBoardingImageThree = "$rootImages/three.PNG";
+  static const String logo = "$rootImages/logoapp.png";
 
   // we need go to AppImageAssets and remove 4th, from static
 }
--- lib/view/screen/auth/login.dart
+++ lib/view/screen/auth/login.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/constants/color.dart';
+import '../../widgets/auth/custombuttonauth.dart';
+import '../../widgets/auth/customtextbodyauth.dart';
+import '../../widgets/auth/customtextformauth.dart';
+import '../../widgets/auth/customtexttitleauth.dart';
+import '../../widgets/auth/logoauth.dart';
+
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -7,9 +14,58 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Title'),
+        centerTitle: true,
+        backgroundColor: AppColor.backgroundcolor,
+        elevation: 0.0,
+        title: Text('Sign In',
+            style: Theme.of(context)
+                .textTheme
+                .displayLarge!
+                .copyWith(color: AppColor.grey)),
       ),
-      body: const Text("Login"),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+        child: ListView(children: [
+          const LogoAuth(),
+          const SizedBox(height: 20),
+          const CustomTextTitleAuth(text: "Welcome Back"),
+          const SizedBox(height: 10),
+          const CustomTextBodyAuth(
+              text:
+                  "Sign In With Your Email And Password OR Continue With Social Media"),
+          const SizedBox(height: 15),
+          const CustonTextFormAuth(
+            hinttext: "Enter Your Email",
+            iconData: Icons.email_outlined,
+            labeltext: "Email",
+            // mycontroller: ,
+          ),
+          const CustonTextFormAuth(
+            hinttext: "Enter Your Password",
+            iconData: Icons.lock_outline,
+            labeltext: "Password",
+            // mycontroller: ,
+          ),
+          const Text(
+            "Forget Password",
+            textAlign: TextAlign.end,
+          ),
+          CustomButtomAuth(text: "Sign In", onPressed: () {}),
+          const SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Don't have an account ? "),
+              InkWell(
+                child: Text("Sign Up",
+                    style: TextStyle(
+                        color: AppColor.primaryColor,
+                        fontWeight: FontWeight.bold)),
+              )
+            ],
+          )
+        ]),
+      ),
     );
   }
 }
