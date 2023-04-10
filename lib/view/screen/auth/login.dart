4c4dfcdb194cb51e2e1e34dbac2934de23c171e3
MohamedAhmedSD <btoox8python@gmail.com>
2023-04-10 14:43:07 +0200

    alert function

--- lib/view/screen/auth/login.dart
+++ lib/view/screen/auth/login.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_wael/core/functions/alertexitapp.dart';
 import 'package:ecommerce_wael/core/functions/validinput.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -33,75 +34,81 @@ class Login extends StatelessWidget {
                 .displayLarge!
                 .copyWith(color: AppColor.grey)),
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-        child: Form(
-          // we bring our key from controller
-          key: controller.formstate,
-          child: ListView(children: [
-            const LogoAuth(),
-            const SizedBox(height: 20),
-            // when use tr == translation ensure you delete const from your widget
-            CustomTextTitleAuth(text: "10".tr), // "Welcome Back"
-            const SizedBox(height: 10),
-            CustomTextBodyAuth(
-                text: "11"
-                    .tr), // "Sign In With Your Email And Password OR Continue With Social Media"
-            const SizedBox(height: 15),
-            CustomTextFormAuth(
-              // use our validator
-              valid: (val) {
-                return validInput(val!, 5, 100, "email");
-              },
-              // kb
-              isNumber: false,
-              // by controller I can listen to what written inside field
-              mycontroller: controller.email,
-              hinttext: "12".tr, // "Enter Your Email"
-              iconData: Icons.email_outlined,
-              labeltext: "18".tr, // "Email"
-              // mycontroller: ,
-            ),
-            CustomTextFormAuth(
-              valid: (val) {
-                return validInput(val!, 5, 30, "password");
-              },
-              isNumber: false,
-              mycontroller: controller.password,
-              hinttext: "13".tr, // "Enter Your Password"
+      // we wrap it under dialog widget 
+      // to tell user when press back is he sure then exit
 
-              iconData: Icons.lock_outline,
-              labeltext: "19".tr, // "Password"
-              // mycontroller: ,
-            ),
-
-            InkWell(
-              onTap: () {
-                // nav by call method that nav => goToForgetPassword
-                controller.goToForgetPassword();
-              },
-              child: Text(
-                "14".tr, // "Forget Password"
-                // we align it to end
-                textAlign: TextAlign.end,
+      body: WillPopScope( // it built0in widget
+        onWillPop: alertExitApp, // take function we make it
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+          child: Form(
+            // we bring our key from controller
+            key: controller.formstate,
+            child: ListView(children: [
+              const LogoAuth(),
+              const SizedBox(height: 20),
+              // when use tr == translation ensure you delete const from your widget
+              CustomTextTitleAuth(text: "10".tr), // "Welcome Back"
+              const SizedBox(height: 10),
+              CustomTextBodyAuth(
+                  text: "11"
+                      .tr), // "Sign In With Your Email And Password OR Continue With Social Media"
+              const SizedBox(height: 15),
+              CustomTextFormAuth(
+                // use our validator
+                valid: (val) {
+                  return validInput(val!, 5, 100, "email");
+                },
+                // kb
+                isNumber: false,
+                // by controller I can listen to what written inside field
+                mycontroller: controller.email,
+                hinttext: "12".tr, // "Enter Your Email"
+                iconData: Icons.email_outlined,
+                labeltext: "18".tr, // "Email"
+                // mycontroller: ,
               ),
-            ),
-            CustomButtomAuth(
-                text: "15".tr, // "Sign In"
-                onPressed: () {
-                  controller.login();
-                }),
-            const SizedBox(height: 40),
-            // nav to opposite
-            CustomTextSignUpOrSignIn(
-              textone: "16".tr, // "Don't have an account ? "
-              texttwo: "17".tr, // "SignUp"
-              onTap: () {
-                // nav by call method that nav to => goToSignUp
-                controller.goToSignUp();
-              },
-            )
-          ]),
+              CustomTextFormAuth(
+                valid: (val) {
+                  return validInput(val!, 5, 30, "password");
+                },
+                isNumber: false,
+                mycontroller: controller.password,
+                hinttext: "13".tr, // "Enter Your Password"
+      
+                iconData: Icons.lock_outline,
+                labeltext: "19".tr, // "Password"
+                // mycontroller: ,
+              ),
+      
+              InkWell(
+                onTap: () {
+                  // nav by call method that nav => goToForgetPassword
+                  controller.goToForgetPassword();
+                },
+                child: Text(
+                  "14".tr, // "Forget Password"
+                  // we align it to end
+                  textAlign: TextAlign.end,
+                ),
+              ),
+              CustomButtomAuth(
+                  text: "15".tr, // "Sign In"
+                  onPressed: () {
+                    controller.login();
+                  }),
+              const SizedBox(height: 40),
+              // nav to opposite
+              CustomTextSignUpOrSignIn(
+                textone: "16".tr, // "Don't have an account ? "
+                texttwo: "17".tr, // "SignUp"
+                onTap: () {
+                  // nav by call method that nav to => goToSignUp
+                  controller.goToSignUp();
+                },
+              )
+            ]),
+          ),
         ),
       ),
     );
--- lib/view/screen/auth/signup.dart
+++ lib/view/screen/auth/signup.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 
 import '../../../controller/auth/signup_controller.dart';
 import '../../../core/constants/color.dart';
+import '../../../core/functions/alertexitapp.dart';
 import '../../../core/functions/validinput.dart';
 import '../../widgets/auth/custombuttonauth.dart';
 import '../../widgets/auth/customtextbodyauth.dart';
@@ -35,79 +36,83 @@ class SignUp extends StatelessWidget {
                 .copyWith(color: AppColor.grey)),
       ),
       body: //GetBuilder<SignUpControllerImp>(
-          Container(
-        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-        child: Form(
-          key: controller.formstate,
-          child: ListView(children: [
-            const SizedBox(height: 20),
-            CustomTextTitleAuth(text: "10".tr), //  "Welcome Back"
-            const SizedBox(height: 10),
-            CustomTextBodyAuth(
-                text: "24"
-                    .tr), //"Sign Up With Your Email And Password OR Continue With Social Media"
-            const SizedBox(height: 15),
-            CustomTextFormAuth(
-              isNumber: false,
-              valid: (val) {
-                return validInput(val!, 3, 20, "username");
-              },
-              // using controller
-              mycontroller: controller.username,
-              hinttext: "23".tr, // "Enter Your Username"
-              iconData: Icons.person_outline,
-              labeltext: "20".tr, // "Username"
-              // mycontroller: ,
-            ),
-            CustomTextFormAuth(
-              isNumber: false,
-              valid: (val) {
-                return validInput(val!, 3, 40, "email");
-              },
-              mycontroller: controller.email,
-              hinttext: "12".tr, // "Enter Your Email"
-              iconData: Icons.email_outlined,
-              labeltext: "18".tr, // "Email"
-              // mycontroller: ,
-            ),
-            CustomTextFormAuth(
-              valid: (val) {
-                return validInput(val!, 7, 11, "phone");
-              },
-              isNumber: true,
-              mycontroller: controller.phone,
-              hinttext: "22".tr, // "Enter Your Phone"
-              iconData: Icons.phone_android_outlined,
-              labeltext: "21".tr, // "Phone"
-              // mycontroller: ,
-            ),
-            CustomTextFormAuth(
-              isNumber: false,
-              valid: (val) {
-                return validInput(val!, 3, 30, "password");
-              },
-              mycontroller: controller.password,
-              hinttext: "13".tr, //"Enter Your Password"
-              iconData: Icons.lock_outline,
-              labeltext: "19".tr, // "Password"
-              // mycontroller: ,
-            ),
-            CustomButtomAuth(
-                text: "Sign Up", // "Sign Up"
-                onPressed: () {
-                  controller.signUp();
-                  // go to success sigup
-                }),
-            const SizedBox(height: 40),
-            // nav to opposite
-            CustomTextSignUpOrSignIn(
-              textone: "25".tr, // " have an account ? "
-              texttwo: "26".tr, // " SignIn "
-              onTap: () {
-                controller.goToSignIn();
-              },
-            ),
-          ]),
+          WillPopScope(
+        // it built0in widget
+        onWillPop: alertExitApp, // take function we make it
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+          child: Form(
+            key: controller.formstate,
+            child: ListView(children: [
+              const SizedBox(height: 20),
+              CustomTextTitleAuth(text: "10".tr), //  "Welcome Back"
+              const SizedBox(height: 10),
+              CustomTextBodyAuth(
+                  text: "24"
+                      .tr), //"Sign Up With Your Email And Password OR Continue With Social Media"
+              const SizedBox(height: 15),
+              CustomTextFormAuth(
+                isNumber: false,
+                valid: (val) {
+                  return validInput(val!, 3, 20, "username");
+                },
+                // using controller
+                mycontroller: controller.username,
+                hinttext: "23".tr, // "Enter Your Username"
+                iconData: Icons.person_outline,
+                labeltext: "20".tr, // "Username"
+                // mycontroller: ,
+              ),
+              CustomTextFormAuth(
+                isNumber: false,
+                valid: (val) {
+                  return validInput(val!, 3, 40, "email");
+                },
+                mycontroller: controller.email,
+                hinttext: "12".tr, // "Enter Your Email"
+                iconData: Icons.email_outlined,
+                labeltext: "18".tr, // "Email"
+                // mycontroller: ,
+              ),
+              CustomTextFormAuth(
+                valid: (val) {
+                  return validInput(val!, 7, 11, "phone");
+                },
+                isNumber: true,
+                mycontroller: controller.phone,
+                hinttext: "22".tr, // "Enter Your Phone"
+                iconData: Icons.phone_android_outlined,
+                labeltext: "21".tr, // "Phone"
+                // mycontroller: ,
+              ),
+              CustomTextFormAuth(
+                isNumber: false,
+                valid: (val) {
+                  return validInput(val!, 3, 30, "password");
+                },
+                mycontroller: controller.password,
+                hinttext: "13".tr, //"Enter Your Password"
+                iconData: Icons.lock_outline,
+                labeltext: "19".tr, // "Password"
+                // mycontroller: ,
+              ),
+              CustomButtomAuth(
+                  text: "Sign Up", // "Sign Up"
+                  onPressed: () {
+                    controller.signUp();
+                    // go to success sigup
+                  }),
+              const SizedBox(height: 40),
+              // nav to opposite
+              CustomTextSignUpOrSignIn(
+                textone: "25".tr, // " have an account ? "
+                texttwo: "26".tr, // " SignIn "
+                onTap: () {
+                  controller.goToSignIn();
+                },
+              ),
+            ]),
+          ),
         ),
       ),
     );
