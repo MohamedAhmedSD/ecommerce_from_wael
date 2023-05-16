026177f416e8129f2f6c3947bbe3ecfe6dfe580e
MohamedAhmedSD <btoox8python@gmail.com>
2023-05-16 16:36:55 +0200

    [7] on boarding page - controller

--- lib/controller/onboarding_controller.dart
+++ lib/controller/onboarding_controller.dart
@@ -1,1 +1,9 @@
-// abstract class
+import 'package:get/get.dart';
+
+//* we need blueprints for what method we will be needed > use abstract
+abstract class OnBoardingController extends GetxController {
+  //* 1. go to next page when press on btn
+  next();
+  //* 2.when pageview move => interact, it need parameter
+  onPageChanged(int index);
+}
