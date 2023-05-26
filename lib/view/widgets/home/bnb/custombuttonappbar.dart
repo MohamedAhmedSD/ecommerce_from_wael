d66e226d7013b904aee908d3031595caedb8cc52
MohamedAhmedSD <btoox8python@gmail.com>
2023-05-26 06:40:13 +0200

    [66] BNB

--- lib/view/widgets/home/bnb/custombuttonappbar.dart
+++ lib/view/widgets/home/bnb/custombuttonappbar.dart
@@ -7,6 +7,8 @@ class CustomButtonAppBar extends StatelessWidget {
   final void Function()? onPressed;
   final String textbutton;
   final IconData icondata;
+  //* for select status
+  //* we can give it inital value from here or on below look at it ===========
   final bool? active;
   const CustomButtonAppBar(
       {Key? key,
@@ -27,6 +29,7 @@ class CustomButtonAppBar extends StatelessWidget {
         Text(
           textbutton,
           style: TextStyle(
+              //*=================================================================
               color: active == true ? AppColor.primaryColor : Colors.black),
         ),
       ]),
